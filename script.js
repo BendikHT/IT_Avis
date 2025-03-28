f3c6ebec1e52fc3e319cd10eaf0149f757928575
Bendik <playstationbendik@gmail.com>
2025-03-28 11:31:14 +0100

    search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const input = document.getElementById("search-bar");
 const resultat_tekst = document.getElementById("resultat-tekst");
-let resultat
+
 document.addEventListener("click", function (event) {
     let article = event.target.closest("article");
     if (article) {
@@ -12,6 +12,7 @@ document.addEventListener("click", function (event) {
 input.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         localStorage.setItem("search", input.value);
-        resultat_tekst.innerHTML = `Søker etter: ${input.value}`;
+        window.open("search.html", "_self");
+        resultat_tekst.innerHTML = localStorage.getItem("search");
     }
 });
