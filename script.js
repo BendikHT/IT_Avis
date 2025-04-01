19489cf7ca7b9efa2a64062d9b53d4c725bb82e3
Bendik <playstationbendik@gmail.com>
2025-04-01 15:23:38 +0200

    små bokstaver

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const input = document.getElementById("search-bar");
-const resultat_tekst = document.getElementById("resultat-tekst");
 
 document.addEventListener("click", function (event) {
     let article = event.target.closest("article");
@@ -9,10 +7,3 @@ document.addEventListener("click", function (event) {
     }
 });
 
-input.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        localStorage.setItem("search", input.value);
-        window.open("search.html", "_self");
-        resultat_tekst.innerHTML = localStorage.getItem("search");
-    }
-});
