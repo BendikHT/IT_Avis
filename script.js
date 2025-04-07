a2d69aaffd2e61514bd66dd7302663d2b22c531d
Bendik <playstationbendik@gmail.com>
2025-04-07 08:43:10 +0200

    oppdaterte liste

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ const articles = [
     { title: "Googles Gemini kan nå se alt du gjør på skjermen din.", url: "artikkler/google-gemini.html" },
     { title: "InnoEX/EFSE 2025: Fremtidens teknologi i Honkong", url: "artikkler/hongkong1.html" },
     { title: "«R.E.P.O» er den nyeste virale spillhitten", url: "artikkler/repo.html" },
-    { title: "Russisk Unit 29155 angriper NATO og allierte nasjoner.", url: "artikkler/russik-hackere.html" }
+    { title: "Russisk Unit 29155 angriper NATO og allierte nasjoner.", url: "artikkler/russik-hackere.html" },
+    { title: "Apple avduker ny AI-drevet MacBook: En milepæl innen bærbare datamaskiner", url: "artikkler/ai_pc.html" },
+    { title: "Selskapet har samlet inn norsk DNA. Nå går de konkurs. Hva skjer med dataene våre da?", url:"konkurs.html"},
+    { title: "Nå er iOS 18.4 her", url:"ios18.html"},
+    { title: "Toppforsker slutter i Google og advarer mot kunstig intelligens", url:"toppforsker.html"},
+    { title: "Nintendo har tre Switch 2-dager denne uken", url:"nintendo2.html"}
 ];
 const dropdown_content = document.getElementById("dropdown-content")
 let search_result_number = 0;
